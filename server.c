d1caffef76faa00b40de8cf1e48df2f0d25b7710
Krueger "Mac" Williams <williamskm@msoe.edu>
2024-12-11 10:26:57 -0600

    First Working Version

--- server.c
+++ server.c
@@ -4,85 +4,63 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <errno.h>  // Include errno for error handling
+#include <errno.h>
 
-#define PORT 8080
 #define BUFFER_SIZE 1024
+#define PROXY_HOST "maccancode.com"  // Replace with your proxy server's IP address
+#define PROXY_PORT 9091  // Port for server connections
 
 int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+    int sock = 0;
+    struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
+    struct hostent *host_entry;
 
-    // Create socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket failed");
+    // Get the proxy server address
+    host_entry = gethostbyname(PROXY_HOST);
+    if (host_entry == NULL) {
+        perror("gethostbyname failed");
         exit(EXIT_FAILURE);
+    }
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
     }
     printf("Socket created successfully\n");
 
-    // Set socket options
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1) {
-        perror("setsockopt failed");
-        close(server_fd);  // Close the socket on error
-        exit(EXIT_FAILURE);
-    }
-    printf("Socket options set successfully\n");
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PROXY_PORT);
+    serv_addr.sin_addr = *((struct in_addr*) host_entry->h_addr);
 
-    // Configure address structure
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // Bind to port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
-        perror("bind failed");
-        close(server_fd);  // Close the socket on error
-        exit(EXIT_FAILURE);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        return -1;
     }
-    printf("Socket bound successfully to port %d\n", PORT);
-
-    // Listen for connections
-    if (listen(server_fd, 3) == -1) {
-        perror("listen failed");
-        close(server_fd);  // Close the socket on error
-        exit(EXIT_FAILURE);
-    }
-    printf("Server is listening on port %d\n", PORT);
+    printf("Connected to the proxy server\n");
 
     while (1) {
-        printf("Waiting for a connection...\n");
-
-        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-        if (new_socket == -1) {
-            perror("accept failed");
-            close(server_fd);  // Optionally close the server socket
-            exit(EXIT_FAILURE);
+        // Send a message to the proxy server
+        strcpy(buffer, "Hello from server");
+        if (send(sock, buffer, strlen(buffer), 0) == -1) {
+            perror("send failed");
+            break;
         }
+        memset(buffer, 0, BUFFER_SIZE);
 
-        printf("Connection established with client\n");
-
-        // Receive and send messages
-        while (1) {
-            int valread = read(new_socket, buffer, BUFFER_SIZE);
-            if (valread <= 0) {
-                if (valread == 0) {
-                    printf("Connection closed by client\n");
-                } else {
-                    perror("read failed");
-                }
-                break;
+        // Receive a message from the proxy server
+        int valread = read(sock, buffer, BUFFER_SIZE);
+        if (valread <= 0) {
+            if (valread == 0) {
+                printf("Connection closed by proxy server\n");
+            } else {
+                perror("read failed");
             }
-            printf("Received: %s\n", buffer);
-            send(new_socket, buffer, strlen(buffer), 0);
-            memset(buffer, 0, BUFFER_SIZE);
+            break;
         }
-
-        close(new_socket);
+        printf("Received from proxy: %s\n", buffer);
     }
 
-    close(server_fd);  // Make sure to close the server socket when done
+    close(sock);
     return 0;
 }
